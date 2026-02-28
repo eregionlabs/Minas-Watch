4aa78e1e7c099b571ded5699125066bc887534ae
Alexa Rose <openclaw624+alexarose@gmail.com>
2026-02-28 12:39:12 -0500

    ops: rotate quick tunnel API endpoint and bump cache version

--- config.js
+++ config.js
@@ -1,3 +1,3 @@
 // Deployed API origin for static GitHub Pages frontend.
 // Note: this is currently a Cloudflare Quick Tunnel (ephemeral).
-window.MINAS_WATCH_API_BASE = "https://avoiding-decisions-months-varying.trycloudflare.com";
+window.MINAS_WATCH_API_BASE = "https://bristol-effects-tunes-colleagues.trycloudflare.com";
